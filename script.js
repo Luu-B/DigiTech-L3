9dc7a49e1e95fe6845013151b166e4dabc8c1507
Luu-B <22002lb@kuranuicollege.school.nz>
2025-09-02 22:14:23 +1200

    cones/tubs

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 50) {
+    nav.style.padding = '5px 30px';
+    nav.style.height = '50px';
+  } else {
+    nav.style.padding = '10px 30px';
+    nav.style.height = '60px';
+  }
+});
+
+document.querySelectorAll('.cone-card').forEach(card => {
+  card.addEventListener('click', () => {
+    alert(`You selected: ${card.querySelector('h3').innerText}`);
+  });
+});

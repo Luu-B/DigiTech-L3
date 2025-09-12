9b24c087c4563272bb39b358d7669b789e7bca27
Luu-B <22002lb@kuranuicollege.school.nz>
2025-09-12 20:08:05 +1200

    Double Flavour
    Code, css, js, etc. all for my double flavour as well as custom double flavour options

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+
 // JS for my Nav
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('nav');
@@ -10,17 +11,44 @@ window.addEventListener('scroll', () => {
     nav.style.height = '60px';
   }
 });
+
+
+
 // JS for my cones / tubs 
 document.querySelectorAll('.cone-card').forEach(card => {
   card.addEventListener('click', () => {
     alert(`You selected: ${card.querySelector('h3').innerText}`);
   });
 });
+
+
+
 // JS for my single flavour
 const flavourCards = document.querySelectorAll('.flavour-card');
 
 flavourCards.forEach(card => {
   card.addEventListener('click', () => {
+    // Remove "selected" from all cards
+    flavourCards.forEach(c => c.classList.remove('selected'));
+
+    // Add "selected" only to the clicked one
+    card.classList.add('selected');
+  });
+});
+
+
+
+// Js for my double flavour choices/custom flavour
+
+const doubleCards = document.querySelectorAll('.double-card');
+const customDouble = document.querySelector('.custom-double');
+
+doubleCards.forEach(card => {
+  card.addEventListener('click', () => {
     card.classList.toggle('selected');
   });
+});
+
+customDouble.addEventListener('click', () => {
+  customDouble.classList.toggle('selected');
 });
